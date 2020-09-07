9f96450531839fc931db8b5f370851845f781b37
Samuel <samuelviviescas@hotmail.com>
2020-09-06 23:01:17 -0500

    estructura de modelo iniciada

---
+++ Tarea integradora 1/src/model/Pedido.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.List;
+
+public class Pedido {
+
+	private String codigo;
+	private int idCliente;
+	private String nitRestnte;
+	private List<Producto> productosApedir;
+	
+	
+	
+	
+	
+	
+}
