1c050f32d29c0ab39b8ecf2b05825f5aefbe99a9
Samuel <samuelviviescas@hotmail.com>
2020-09-10 00:34:32 -0500

    creacion de las clases menu

---
+++ Tarea integradora 1/src/ui/ClienteMenu.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class ClienteMenu {
+	
+	public ClienteMenu() {
+	}
+
+}
--- Tarea integradora 1/src/ui/MainMenu.java
+++ Tarea integradora 1/src/ui/MainMenu.java
@@ -1,5 +1,21 @@
 package ui;
 
-public class Menu {
+public class MainMenu {
 
+	private RestauranteMenu resMenu;
+	private ClienteMenu cliMenu;
+	private ProductoMenu proMenu;
+	private PedidoMenu pedMenu;
+	
+	public MainMenu() {
+		resMenu = new RestauranteMenu();
+		cliMenu = new ClienteMenu();
+		proMenu = new ProductoMenu();
+		pedMenu = new PedidoMenu();
+	}
+	
+	public void startMainMenu() {
+		
+	}
+	
 }
